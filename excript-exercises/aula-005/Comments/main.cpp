f2601eb8420528d3080d7e91a02947fcfcca4f15
dognew <dognew@gmail.com>
2019-07-29 01:18:07 -0300

    Class 06 - Comments

---
+++ excript-exercises/aula-005/Comments/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+* Este tipo de comentário de várias linhas.
+* Sempre abra-o e feche.
+*/
+
+int main()
+{
+    // A mensagem após as barras não será interpretado.
+    // Isto é usado para comentário de uma linha
+    cout << "Ola Mundo!" << endl;
+    return 0;
+}
