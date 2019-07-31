d2653f58fb3ff9f594d03a85827448562ccc299e
dognew <dognew@gmail.com>
2019-07-31 15:31:55 -0300

    Add docs

---
+++ docs/webpages/js/slides.js
@@ -0,0 +1,10 @@
+$(function() {
+	$("#sliderClasses, #sliderWeeks").responsiveSlides({
+		auto: true,
+		pager: false,
+		nav: true,
+		speed: 500,
+		namespace: "large-btns",
+		maxwidth: "992"
+	});
+});
